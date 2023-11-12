4d255d25ecdfadddd11722c3e5ec2b4e2b8a656d
Mostafadars <92469012+Mostafadars@users.noreply.github.com>
2023-11-12 12:02:37 +0200

    Add GasBill Class With his methods

--- GasBill.java
+++ GasBill.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class GasBill {
+public class GasBill extends Bill{
+    public GasBill(float amount, String date, Boolean status) {
+        super(amount, date, status);
+    }
+
+    @Override
+    public void printBill() {
+        // method to print gas bill
+        System.out.println("Gas bill: " + this.getAmount());
+    }
+
+    @Override
+    public void createBill() {
+        // method to generate random gas bill  between 50 and 100
+        float amount = (float) (Math.random() * (100 - 50 + 1) + 50);
+        this.setAmount(amount);
+    }
+
+    @Override
+    public void deducateBill() {
+        // method to deduct gas bill from bank account
+    }
 }
