3de60e4a74896af52ac10d2ef2d2c28cce62c3c2
Mostafadars <92469012+Mostafadars@users.noreply.github.com>
2023-11-12 11:55:04 +0200

    Add Bill Class

---
+++ Bill.java
@@ -0,0 +1,30 @@
+public abstract class Bill {
+    private float amount;
+    private String date;
+    private Boolean status;
+
+    public Bill(float amount, String date, Boolean status) {
+        this.amount = amount;
+        this.date = date;
+        this.status = status;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public Boolean getType() {
+        return status;
+    }
+
+    public abstract void printBill();
+
+    public abstract void createBill();
+
+    public abstract void deducateBill();
+
+}
