82157d86a4f4bb553afa26706981b9fab72fb72d
Mostafadars <92469012+Mostafadars@users.noreply.github.com>
2023-11-12 11:45:52 +0200

    Add User Class

---
+++ User.java
@@ -0,0 +1,35 @@
+public class User {
+    private String name;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private String address;
+
+    public User(String name, String email, String password, String phoneNumber, String address) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+public String getAddress() {
+        return address;
+    }
+}
