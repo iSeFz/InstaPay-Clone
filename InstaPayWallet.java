7eefa6811bc6f2e6ebd38a4592afffde3ffd7867
‪Yousef Hussien Awad‬‏ <99095223+yousefhussien99@users.noreply.github.com>
2023-11-13 20:13:06 +0200

    Delete InstaPayWallet.java

--- InstaPayWallet.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-public abstract class InstaPayWallet extends InstaPayAccount {
-    public InstaPayWallet(User user, double balance) {
-        super(user, balance);
-    }
-    public abstract void transferMoney(List<InstaPayAccount> instaPayAccounts);
-}
