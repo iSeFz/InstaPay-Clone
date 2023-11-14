ed4409d80d43ea851aefb1f3d6816cec5bf34178
Oppadnasser <oppadnasser@gmail.com>
2023-11-14 14:30:58 +0200

    implement verifyPhoneNumber function

--- Registration.java
+++ Registration.java
@@ -22,6 +22,30 @@ public abstract class Registration {
     }
     abstract public boolean verifyAccount(InstaPayAccount user);
     public boolean verifyMobilNumber(User user){
+        String mobile;
+        System.out.println("Please Enter Your Phone Number:");
+        mobile = in.nextLine();
+        while(!mobile.matches("^01[0125]\\d{8}$")){
+            System.out.println("Please Enter Valid Mobile Number or 0 To Cancel Registration:");
+            mobile = in.nextLine();
+            if(mobile.equals("0")){
+                return false;
+            }
+        }
+        String Otp;
+        OTP otp = new OTP();
+        otp.generateOTP();
+        otp.sendOTP();
+        System.out.println("Please Enter Otp recently send:");
+        Otp = in.nextLine();
+        while (!otp.verifyOTP(Otp)){
+            System.out.println("Please Enter correct OTP or 0 to Cancel Registration:");
+            Otp = in.nextLine();
+            if(Otp.equals("0")){
+                return false;
+            }
+        }
+        user.setPhoneNumber(mobile);
         return true;
     }
     public boolean verifyPassword(User user){
