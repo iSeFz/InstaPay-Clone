7fdc18449d5aa208d1c292021e6798a87f657caf
iSeFz <seifyahia@gmail.com>
2023-11-15 01:14:14 +0200

    Renamed InstaPayAccount class to Account

--- Account.java
+++ Account.java
@@ -1,22 +1,28 @@
 import java.util.List;
 import java.util.Scanner;
 
-public abstract class InstaPayAccount {
+public abstract class Account {
     User user;
     double balance;
-    public InstaPayAccount(User user, double balance) {
+
+    public Account(User user, double balance) {
         this.user = user;
         this.balance = balance;
     }
-    // this function to set account number in bank account  case
+
+    // this function to set account number in bank account case
     // and set mobile number of wallet in mobile wallet case
     abstract public void setbase(String number);
+
     abstract public String getbase();// to return account bank number or mobile wallet number
-    abstract public void transferMoney(List<InstaPayAccount> instaPayAccounts );
-    public User getUser(){
+
+    abstract public void transferMoney(List<Account> instaPayAccounts);
+
+    public User getUser() {
         return user;
     }
-    public void setUser(User u){
+
+    public void setUser(User u) {
         user.setName(u.getName());
         user.setPassword(u.getPassword());
         user.setEmail(u.getEmail());
@@ -24,7 +30,7 @@ public abstract class InstaPayAccount {
         user.setAddress(u.getAddress());
     }
 
-    public void inquireBalance(){
+    public void inquireBalance() {
         System.out.println("Your Current Balance is: " + this.balance);
     }
 
@@ -53,6 +59,7 @@ public abstract class InstaPayAccount {
             } else {
                 System.out.println("Bill Not Paid");
             }
+            input.close();
         }
     }
 }
