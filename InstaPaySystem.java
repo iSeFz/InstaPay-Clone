294872aac5eac14458851f5dcabf8570c28d01e6
iSeFz <seifyahia@gmail.com>
2023-11-14 18:57:17 +0200

    Modified runSystem method & added subMenu method

--- InstaPaySystem.java
+++ InstaPaySystem.java
@@ -1,8 +1,11 @@
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-//new version
+
 public class InstaPaySystem {
+    private List<InstaPayAccount> accounts;
+
+    /* 
+    ##### For TESTING Purposes #####
     private List<InstaPayAccount> instaPayAccounts;
     public InstaPaySystem() {
         instaPayAccounts = new ArrayList<>();
@@ -37,9 +40,39 @@ public class InstaPaySystem {
                 System.out.println();
             }
         }
+    } */
+
+    // Sub menu that appear after logging in to a certain account
+    private void subMenu(InstaPayAccount account) {
+        System.out.println("\tWelcome " + account.user.getName() + " to InstaPay Clone!");
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("\n1. Transfer Money\n" +
+                    "2. Inquire Balance\n" +
+                    "3. Pay Bills");
+            System.out.print("Enter your choice (1-3): ");
+            String choice = sc.nextLine();
+            if (choice.equals("1")) {
+                account.transferMoney(accounts);
+            } else if (choice.equals("2")) {
+                // account.inquireBalance();
+                System.out.println("Inquiring Balance! Coming Soon!");
+                break;
+            } else if (choice.equals("3")) {
+                // account.payBills();
+                System.out.println("\tPaying Bills! Coming Soon!");
+                break;
+            } else {
+                System.out.println("\n\tINVALID CHOICE");
+            }
+        }
+        sc.close();
     }
+
+    // Main menu that appear at the start of the program
     public void runSystem() {
         Scanner sc = new Scanner(System.in);
+        InstaPayAccount account = null;
         System.out.println("\tWelcome to InstaPay Clone!");
         while (true) {
             System.out.println("\n1. Register a new account\n" +
@@ -50,12 +83,17 @@ public class InstaPaySystem {
             if (choice.equals("1")) {
                 System.out.println("\tRegistering a new account...");
             } else if (choice.equals("2")) {
-                if(accounts == null){
+                if (accounts != null) {
+                    Login loginObj = new Login();
+                    account = loginObj.login(accounts);
+                    if (account != null) {
+                        subMenu(account);
+                    } else {
+                        System.err.println("\n\tLogin Failed!");
+                    }
+                } else {
                     System.err.println("\tNo account found, Register a new account first!");
-                    continue;
                 }
-                Login loginObj = new Login();
-                loginObj.login(accounts);
             } else if (choice.equals("3")) {
                 System.out.println("\tThank you for using InstaPay Clone!");
                 break;
@@ -65,6 +103,7 @@ public class InstaPaySystem {
         }
         sc.close();
     }
+
     public static void main(String[] args) {
         InstaPaySystem sys = new InstaPaySystem();
         sys.runSystem();
