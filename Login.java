251418d765eb1c5f6596a30b115a7fa644e66e99
iSeFz <seifyahia@gmail.com>
2023-11-14 18:56:46 +0200

    Modified the Login class to return account

--- Login.java
+++ Login.java
@@ -3,35 +3,43 @@ import java.util.List;
 
 public class Login {
     // Login to an existing account
-    public boolean login(List<InstaPayAccount> accounts) {
+    public InstaPayAccount login(List<InstaPayAccount> accounts) {
         System.out.println("\n\tLogin To Your Account");
+        InstaPayAccount loggedIn = null;
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your username: ");
         String username = sc.nextLine();
+        loggedIn = checkUsername(username, accounts);
+        if(loggedIn == null){
+            sc.close();
+            return null;
+        }
         System.out.print("Enter your password: ");
         String password = sc.nextLine();
-        if(checkAccount(username, password, accounts)){
-            System.out.println("\tLogin Successfull!");
-            sc.close();
-            return true;
-        }
+        loggedIn = checkPassword(password, loggedIn);
         sc.close();
-        return false;
+        return loggedIn;
     }
 
-    // Check for account if it is stored before in the system
-    public boolean checkAccount(String username, String password, List<InstaPayAccount> accounts) {
+    // Check if username is registered to an account before
+    public InstaPayAccount checkUsername(String username, List<InstaPayAccount> accounts) {
         for (InstaPayAccount acc : accounts) {
             if (username.equals(acc.getUser().getName())) {
-                if(password.equals(acc.getUser().getPassword())){
-                    return true;
-                } else {
-                    System.err.println("\tPassword is incorrect!");
-                    return false;
-                }
+                return acc;
             }
         }
         System.err.println("Username not found, Register a new account first!");
-        return false;
+        return null;
+    }
+
+    // Check for account if it is stored before in the system
+    public InstaPayAccount checkPassword(String password, InstaPayAccount account) {
+        if(password.equals(account.getUser().getPassword())){
+            System.out.println("\n\tLogin Successfull!");
+            return account;
+        } else {
+            System.err.println("\tPassword is incorrect!");
+            return null;
+        }
     }
 }
