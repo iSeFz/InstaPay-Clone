e485064970b2fe6cbe6e31c6434cd40114a5f291
Oppadnasser <oppadnasser@gmail.com>
2023-11-14 14:13:08 +0200

    implemnt verifyEmail funcion

--- Registration.java
+++ Registration.java
@@ -1,10 +1,23 @@
 import java.util.List;
+import java.util.Scanner;
 
 public abstract class Registration {
+    public Scanner in = new Scanner(System.in);
     public boolean register(InstaPayAccount user, List<InstaPayAccount> list) {
         return true;
     }
     public boolean verifyEmail(User user){
+        String mail;
+        System.out.println("Please Enter You Email");
+        mail = in.nextLine();
+        while(!mail.matches("^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$")){
+            System.out.println("Invalid Email\nPlease Enter valid mail or 0 to cancel:");
+            mail = in.nextLine();
+            if(mail.equals("0")){
+                return false;
+            }
+        }
+        user.setEmail(mail);
         return true;
     }
     abstract public boolean verifyAccount(InstaPayAccount user);
