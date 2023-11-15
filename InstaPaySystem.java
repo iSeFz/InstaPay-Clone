2d1a6931b84cedce36703404f23dcb1e624c47a9
iSeFz <seifyahia@gmail.com>
2023-11-15 19:04:54 +0200

    Updated the system class with the required changes Still not working properly, because of an I/O error I can't solve

--- InstaPaySystem.java
+++ InstaPaySystem.java
@@ -3,15 +3,11 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InstaPaySystem {
-    private List<Account> accounts;
+    private List<Account> accounts = new ArrayList<>();
 
-    public InstaPaySystem() {
-        accounts = new ArrayList<>();
-    }
-
-    // NOTE: This violates the OCP, but it was done for better readability
     // Registration helper method for more readable code
-    private void registrationHelper(Account account, List<Account> accounts) {
+    private void registrationHelper() {
+        Account account = null;
         Scanner sc = new Scanner(System.in);
         while (true) {
             System.out.println("Which account type do you want to register?\n" +
@@ -21,23 +17,25 @@ public class InstaPaySystem {
             String accountType = sc.nextLine();
             if (accountType.equals("1")) {
                 RegistrationBankAccount newBankAccount = new RegistrationBankAccount();
-                if (newBankAccount.register(account, accounts)) {
-                    subMenu(account);
-                    break;
-                } else
-                    break;
+                account = newBankAccount.register(accounts);
+                break;
             } else if (accountType.equals("2")) {
                 RegistrationMobileWallet newWalletAccount = new RegistrationMobileWallet();
-                if (newWalletAccount.register(account, accounts)) {
-                    subMenu(account);
-                    break;
-                } else
-                    break;
+                account = newWalletAccount.register(accounts);
+                break;
             } else {
                 System.err.println("\n\tINVALID CHOICE");
             }
         }
         sc.close();
+        // If the registration was successfull
+        // Add the user to the list of accounts stored on the system
+        if (account != null) {
+            accounts.add(account);
+            subMenu(account);
+        } else {
+            System.out.println("\n\tRegistration Failed!");
+        }
     }
 
     // Sub menu that appear after logging in to a certain account
@@ -56,50 +54,50 @@ public class InstaPaySystem {
             } else if (choice.equals("2")) {
                 account.inquireBalance();
             } else if (choice.equals("3")) {
-                Bill billToPay = null;
-                account.payBill(billToPay); // Here Must be Modified ==> Talk With Mostafa
+                account.payBill(); // Here Must be Modified ==> Talk With Mostafa
             } else if (choice.equals("4")) {
+                sc.close();
                 break;
             } else {
-                System.out.println("\n\tINVALID CHOICE");
+                System.err.println("\n\tINVALID CHOICE");
             }
         }
-        sc.close();
     }
 
     // Main menu that appear at the start of the program
     public void runSystem() {
         Scanner sc = new Scanner(System.in);
+        String choice = "";
         Account account = null;
         System.out.println("\tWelcome to InstaPay Clone!");
-        while (true) {
+        while (!choice.equals("3")) {
             System.out.println("\n1. Register a new account\n" +
                     "2. Login into existing account\n" +
                     "3. Exit");
             System.out.print("Enter your choice (1-3): ");
-            String choice = sc.nextLine();
+            choice = sc.nextLine();
             if (choice.equals("1")) {
                 System.out.println("\n\tRegistering a new account");
-                registrationHelper(account, accounts);
+                registrationHelper();
+                break;
             } else if (choice.equals("2")) {
                 if (accounts != null) {
                     Login loginObj = new Login();
                     account = loginObj.login(accounts);
                     if (account != null) {
                         subMenu(account);
+                        break;
                     } else {
                         System.err.println("\n\tLogin Failed!");
                     }
                 } else {
                     System.err.println("\tNo account found, Register a new account first!");
                 }
-            } else if (choice.equals("3")) {
-                System.out.println("\tThank you for using InstaPay Clone!");
-                break;
             } else {
                 System.err.println("\n\tINVALID CHOICE");
             }
         }
+        System.out.println("\tThank you for using InstaPay Clone!");
         sc.close();
     }
 
